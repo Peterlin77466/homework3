92813126817f52c95697f6ac10e33237d59bf2f3
Peterlin77466 <linpeitaopeter@gmail.com>
2022-10-26 18:07:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+a=int(input("how many friend?"))
+b=int(input("how many candy"))
+share=(b // a)
+remainder=(b%a)
+print("You should give",share,"chocolates(s) to each friend and you will have",remainder,"left for you.")
+quit()
